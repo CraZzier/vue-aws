cd66781d17cf2de479eefaeb161c3fe50798804b
CraZzier <jasgrzegorek@gmail.com>
2024-07-21 23:56:11 +0200

    another test

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,6 +12,17 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/js/main.js')
+      },
+      output: {
+        format: 'iife', // Immediately Invoked Function Expression (IIFE) for non-module script compatibility
+        dir: 'dist/js'
+      }
     }
   }
 })
